fd1b1911a7e2589d4959b984106c86d714fd3d10
christianauz <67845460+christianauz@users.noreply.github.com>
2020-07-10 20:42:15 -0500

    graficas de prueba tab1
    graficas de prueba tab1

--- App/tabs/tab1.py
+++ App/tabs/tab1.py
@@ -3,7 +3,7 @@ import plotly
 import dash_core_components as dcc
 import dash_html_components as html 
 import dash_bootstrap_components as dbc 
-import dash_table
+#import dash_table
 import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
@@ -15,15 +15,25 @@ from database import transforms
 
 #df = transforms.df
 figpie = transforms.figpie
+fig = transforms.fig
+fig2 = transforms.fig2
+fig1 = transforms.fig1
 
-PAGE_SIZE = 50
+#PAGE_SIZE = 50
 #  html.Div([
 #         dbc.Row([dbc.Col(html.Div(html.P("A single, half-width column")),style = {'padding':'50px'})
 #                 ,dbc.Col(
-layout =html.Div(  #html.H1('Tab uno contenido'),
-				dcc.Graph(figure=figpie, id='pie_grap2')
+layout =html.Div([
+
+ 			#html.H3('Tab content 1'),
+            #html.H3('Tab content 3'),
+
+				  #html.H1('Tab uno contenido'),
+				#html.H1(children='Hello Dash'),
+			dcc.Graph(figure=fig2, id='g1'),
+			dcc.Graph(figure=fig1, id='g2'),
+			dcc.Graph(figure=fig, id='g3')
+				#html.Div([dcc.Graph(figure=fig1, id='pie_gr')])
 		
-                        )
-            #             , width=9)
-            #     ])
-            # ])
+                        
+])
