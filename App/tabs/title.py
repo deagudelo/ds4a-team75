572219fdd56577cd19d515df4cc6e463febc8d8d
deagudelo <de.agudelo@uniandes.edu.co>
2020-07-18 13:49:22 -0500

    nuevos tabs

--- App/tabs/title.py
+++ App/tabs/title.py
@@ -1,32 +1,24 @@
-import dash
-import plotly
-import dash_core_components as dcc
-import dash_html_components as html 
-import dash_bootstrap_components as dbc 
-import dash_table
-import pandas
-from dash.dependencies import Input, Output
-
+import dash_html_components as html
 from app import app
 
-from tabs import tab1, tab2
-from database import transforms
 
-
-ti = html.Div([
-    
-    html.Div([
-    html.Div(
+ti = html.Div(
+    className='row justify-content-center',
+    children=[
+        html.Div(
             children=[
                 html.Img(
                     src=app.get_asset_url("logoepm.png"),
                     id="epm-image",
                 ),
             ],
-    className="two columns"),
-    html.Div([
-    html.H1('Electricity Distribution Failure Model') 
-
-    ], className="ten columns"),
-    ], className="row")
-])
+            className="col-xs-10 col-md-2"),
+        html.Div(
+            children=[
+                html.H1(
+                    'Electricity Distribution Failure Model',
+                    className="mx-auto text-center"
+                )
+            ], className="col-xs-10")
+    ]
+)
