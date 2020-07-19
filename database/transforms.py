ccf06c8e0bf323da9c5c7bce09224a04f48e6451
deagudelo <de.agudelo@uniandes.edu.co>
2020-07-19 00:04:28 -0500

    más bugs

--- database/transforms.py
+++ database/transforms.py
@@ -13,7 +13,7 @@ from pprint import pprint as pp
 print("cwd:")
 print(os.getcwd())
 # pp(vars(app))
-path = os.path.join(os.getcwd(), 'uraba_all.xlsx')
+path = os.path.join(os.getcwd(), 'database', 'uraba_all.xlsx')
 print(path)
 df = pd.read_excel(path)
 df.Latitude = df.Latitude/1000000
@@ -22,7 +22,7 @@ df.Longitude = df.Longitude/1000000
 num_service_type=df["ServiceType"].value_counts().reset_index()
 figpie= px.pie(num_service_type, values='ServiceType', names='index', title='Service Type Distribution')
 
-locres_wo = pd.read_csv(os.path.join(os.getcwd(), 'locres_wo.csv'))
+locres_wo = pd.read_csv(os.path.join(os.getcwd(), 'database', 'locres_wo.csv'))
 figpie_comp = px.pie(locres_wo, values='LocationResume', names='index', title='Failure components ditribution')
 
 num_repar_ot=df["NumberOT"].value_counts().reset_index() 
