fd860514cfc51f0f478a9a0734882214175a0399
christianauz <67845460+christianauz@users.noreply.github.com>
2020-07-31 08:56:31 -0500

    Cambios formato, filtros en construccion

--- tabs/navbar.py
+++ tabs/navbar.py
@@ -1,22 +1,50 @@
 # -*- coding: utf-8 -*-
-
 import dash_bootstrap_components as dbc
-def Navbar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink("Home", href="/index")),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem("Page 2", href="#"),
-                    dbc.DropdownMenuItem("Page 3", href="#"),
-                ],
-                nav=True,
-                in_navbar=True,
-                label="More",
+import dash_html_components as html
+#from application import application
+
+
+nav_item = dbc.NavItem(dbc.NavLink(html.H2("DS4A"), href="#"))
+
+
+
+
+#def layout(application):
+  #  return
+def Navbar(application):
+    navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+
+
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [   
+
+                        dbc.Col(html.Img(src=application.get_asset_url("Logo_EPM_blanco.png"), height="50px")),
+                        dbc.Col(dbc.NavbarBrand(html.H2("Electricity Distribution Failure Model",  style={'color': '#284730'}), className="ml-2")),
+                    ],
+                    align="center",
+                    no_gutters=True,
+                ),
+                #href="",
             ),
-        ],
-        brand="Empresa de servicios públicos de Medellín y Colombia",
-        color="#83ac07",
-        dark=True,
+
+
+            #dbc.NavbarToggler(id="navbar-toggler2"),
+            dbc.Collapse(
+                dbc.Nav(
+                    [nav_item], className="ml-auto", navbar=True
+                ),
+                id="navbar-collapse2",
+                navbar=True,
+            ),
+        ]
+    ),
+    color="#83ac07",
+
+    #dark=True,
+    #className="mb-5",
     )
     return navbar
--- tabs/sidepanel.py
+++ tabs/sidepanel.py
@@ -1,22 +1,26 @@
 import dash_html_components as html
 import dash_core_components as dcc
 from database import transforms
+import dash_bootstrap_components as dbc
 
 listaTechLocation = ['Transformador', 'Salida Circuito',
                      'Aisladero', 'Reconectador', 'Salida_Circuito']
 #df = transforms.df
 df_localidad = transforms.df_localidad
 
-layout = html.Div(
+layout = html.Div( 
     [
-        html.H2('Filters'),
-        html.H4('Technical location:'),
-        html.Br(),
+          
+        
+        html.H3('Dashboard filters:'),
+        html.H4('Technical Location:'),
+        #html.Br(),
         dcc.Dropdown(
             id='techlocation',
             options=[{'label': i, 'value': i} for i in listaTechLocation],
             value='Transformador'
         ),
+           html.H4('Town:'),
                                 html.Div(
                                     className="div-for-dropdown",
                                     children=[
@@ -33,8 +37,11 @@ layout = html.Div(
 
 
 
-        html.Div([html.H5('Controls')])
-    ],
+        html.Div([html.H5('Controls')]) 
+    ],  
+
+    
     style={'marginBottom': 50, 'marginTop': 25,
-           'marginLeft': 15, 'marginRight': 15}
+           'marginLeft': 15, 'marginRight': 15,
+           }
 )
